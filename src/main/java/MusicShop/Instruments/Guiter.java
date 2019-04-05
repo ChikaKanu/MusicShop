046b1ba326cd026594e95e8ecb8273f9bfb014eb
Ifeyinwa Kanu <chika.kanu@procompletions.com>
2019-04-05 16:56:31 +0100

    guiter add and get name complete

--- src/main/java/MusicShop/Instruments/Guiter.java
+++ src/main/java/MusicShop/Instruments/Guiter.java
@@ -1,4 +1,32 @@
 package MusicShop.Instruments;
 
-public class Guiter {
+import MusicShop.Enums.GuiterType;
+
+public class Guiter extends Instrument{
+
+    int noOfStrings;
+    GuiterType guiterType;
+
+    public Guiter(String name, double purchasePrice, double sellingPrice, boolean inStock, String colour, int noOfStrings, GuiterType guiterType) {
+        super(name, purchasePrice, sellingPrice, inStock, colour);
+        this.noOfStrings = noOfStrings;
+        this.guiterType = guiterType;
+    }
+
+    public int getNoOfStrings() {
+        return noOfStrings;
+    }
+
+    public void setNoOfStrings(int noOfStrings) {
+        this.noOfStrings = noOfStrings;
+    }
+
+    public GuiterType getGuiterType() {
+        return guiterType;
+    }
+
+    public void setGuiterType(GuiterType guiterType) {
+        this.guiterType = guiterType;
+    }
+
 }
--- src/test/java/GuiterTest.java
+++ src/test/java/GuiterTest.java
@@ -1,2 +1,24 @@
+import MusicShop.Enums.GuiterType;
+import MusicShop.Instruments.Guiter;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuiterTest {
+
+    Guiter guiter;
+
+    @Before
+    public void before() {
+        guiter = new Guiter("Guiter", 100, 120, true, "wood", 4, GuiterType.ACCUSTIC);
+    }
+
+    @Test
+    public void canSetGetName() {
+        guiter.setName("GermoGuiter");
+        assertEquals("GermoGuiter", guiter.getName());
+    }
+
+
 }
