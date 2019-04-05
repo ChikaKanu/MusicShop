8a237b5ad6db0ef17caf86a4bba2d4fd379775e2
Ifeyinwa Kanu <chika.kanu@procompletions.com>
2019-04-05 16:41:36 +0100

    complete getters and setters for instruments and parts abstract classes not test

--- src/main/java/MusicShop/Parts/Part.java
+++ src/main/java/MusicShop/Parts/Part.java
@@ -1,5 +1,21 @@
 package MusicShop.Parts;
 
-public class Part {
+import MusicShop.ShopItem;
 
+public class Part extends ShopItem {
+
+    private String material;
+
+    public Part(String name, double purchasePrice, double sellingPrice, boolean inStock, String material) {
+        super(name, purchasePrice, sellingPrice, inStock);
+        this.material = material;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
 }
